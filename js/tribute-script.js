40b55bff901d65aafdaf778e333393923fcf446c
Bonface Mudiri <mudirib@gmail.com>
2017-05-13 11:41:13 +0300

    Modify js/tribute-script.js JQuery script and modify audio trigger prop

--- js/tribute-script.js
+++ js/tribute-script.js
@@ -7,19 +7,57 @@
 
 $(function (){
 
+		$("audio").trigger("pause");
+	    $("audio").prop("loop", true);
+
+
 	$(document).ready(function (){
+
+		$("#zero").click(function(){
+			$("audio").trigger("play");
+			$("audio").prop("volume", 0.3);
+		});
+
+		$("#finito").click(function(){
+			$("audio").trigger("pause");
+		});
+
+	
+
+		$('body').addClass('hide-scroll');
+
+		/*$('#finito').hide();*/
+
+		$('#hyperLink').addClass('hide-hyperlink');
+		$("#extra").addClass("hide-btn-extra");
+
+		$("#zero").css("border-bottom", "none");
+
+		$("#finito").css("border-bottom", "none");
+
+
+		const height = $(window).height();
 
 		$(window).scroll(function (){
 			if($(window).scrollTop() < 1){
 				$('#nav').show();
-				$('#yearLink').css('margin-top', '70px');
+				$('#yearLink').css('margin-top', '50px');
 
+			}
+
+			if($(window).scrollTop() > height){
+				$('#nav').hide();
+				$('#yearLink').css('margin-top', '0');
+				$(".right").addClass("add-transform");
 			}
 		});
 
-		
-
 		$("a").click(function(event){
+
+			$('#hyperLink').removeClass('hide-hyperlink');
+			$("#extra").addClass("hide-btn-extra");
+
+
 
 			if(this.hash !== ""){
 				event.preventDefault();
@@ -28,14 +66,37 @@ $(function (){
 
 				$('html, body').animate({
 					scrollTop: $(hash).offset().top
-				}, 800, function(){
+				}, 1000, function(){
 					window.location.hash = hash;
 				});
 				$('#nav').hide();
 				$('#yearLink').css('margin-top', '0');
+				$('#finito').show();
+				$("#right1").css("transition","all 15s ease");
 			}
 
+
 		});
+
+		$("#finito").click(function(){
+			$("#hyperLink").addClass("hide-hyperlink");
+			$("#extra").removeClass("hide-btn-extra");
+
+		});
+
+		$("#readMore").click(function(){
+			$("#extra").removeClass("hide-btn-extra");
+
+		});
+
+			$(".right").hover(function(){
+				$(this).removeClass("add-transform");
+			});
+
+			$(".left").hover(function(){
+				$(".right").removeClass("add-transform");
+			});
+
 
 
 	});
